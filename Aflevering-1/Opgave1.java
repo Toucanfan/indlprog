1f1b13e2f89c5567be3dba0135cc93da1fc9e3bc
Henrik Enggaard <henrik.enggaard@gmail.com>
2014-02-05 09:51:13 +0100

    Begyndt på opgave 1

---
+++ Aflevering-1/Opgave1.java
@@ -0,0 +1,11 @@
+public class Opgave1 {
+	public static void main(String[] args) {
+		printMessage();
+		System.out.println("Remember:");
+		printMessage();
+	}
+	
+	public static void printMessage() {
+		System.out.println("Use \"\\\\\" to obtain a ’backslash’ character.");
+	}
+}
