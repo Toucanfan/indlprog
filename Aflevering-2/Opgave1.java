08afa4693dd22bcbe906a6977b4c6792e7449904
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2014-02-19 08:28:28 +0100

    Begyndt på opgave 1

---
+++ Aflevering-2/Opgave1.java
@@ -0,0 +1,11 @@
+public class RomanNumerals {
+	public static void main(String[] args) {
+		
+	}
+
+	public static String toRomanNumerals(int number) {
+		String romanNumeral = "";
+		
+		return romanNumeral;
+	}
+}
