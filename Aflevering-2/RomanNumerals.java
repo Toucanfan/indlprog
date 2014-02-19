cd0fe007f7319b342926856c6cad0c3f8814461b
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2014-02-19 08:56:07 +0100

    Test af funktionen

--- Aflevering-2/RomanNumerals.java
+++ Aflevering-2/RomanNumerals.java
@@ -1,11 +1,28 @@
 public class RomanNumerals {
 	public static void main(String[] args) {
-		
+		test("", toRomanNumerals(0));
+		test("I", toRomanNumerals(1));
+		test("II", toRomanNumerals(2));
+		test("V", toRomanNumerals(5));
+		test("IV", toRomanNumerals(4));
+		test("VI", toRomanNumerals(6));
+		test("X", toRomanNumerals(10));
 	}
 
 	public static String toRomanNumerals(int number) {
 		String romanNumeral = "";
 
 		return romanNumeral;
+	}
+
+	public static void test(String target, String test) {
+		if (target != test) {
+			System.out.println("Expected: " + target);
+			System.out.println("Got: " + test);
+		} else {
+			System.out.println("Passed!");
+		}
+
+		System.out.println("");
 	}
 }
