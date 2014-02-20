bef5811ce302032d4373a8ad082ee775488ad242
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2014-02-20 21:15:26 +0100

    Mere general løsning

--- Aflevering-2/RomanNumerals.java
+++ Aflevering-2/RomanNumerals.java
@@ -6,32 +6,44 @@ public class RomanNumerals {
 		test("V", toRomanNumerals(5));
 		test("IV", toRomanNumerals(4));
 		test("VI", toRomanNumerals(6));
+
 		test("X", toRomanNumerals(10));
+		test("XX", toRomanNumerals(20));
+		test("L", toRomanNumerals(50));
+		test("XL", toRomanNumerals(40));
+		test("LX", toRomanNumerals(60));
+		test("XC", toRomanNumerals(90));
 	}
 
 	public static String toRomanNumerals(int number) {
 		String romanNumeral = "";
 
-		int digit = number % 10;
+		int digit1 = number % 10;
 
-		switch (digit) {
-			case 5:
-				romanNumeral += "V";
-				break;
-			case 4:
-				romanNumeral += "IV";
-				break;
-			case 9:
-				romanNumeral += "IX";
-				break;
-			default:
-				for (int i = 0; i < digit; i++){
-					romanNumeral += "I";
-				}
-				break;
-		}
+		romanNumeral += generalNumeral(digit1, "I", "V", "X");
+
+		int digit10 = (number / 10) % 10;
+
+		romanNumeral += generalNumeral(digit10, "X", "L", "C");
 
 		return romanNumeral;
+	}
+
+	public static String generalNumeral(int digit, String oner, String fiver, String tenner) {
+		switch (digit) {
+			case 5:
+				return fiver;
+			case 4:
+				return oner + fiver;
+			case 9:
+				return oner + tenner;
+			default:
+				String _tmp = "";
+				for (int i = 0; i < digit; i++){
+					 _tmp += oner;
+				}
+				return _tmp;
+		}
 	}
 
 	public static void test(String target, String test) {
