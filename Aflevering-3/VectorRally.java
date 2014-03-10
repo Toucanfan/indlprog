fd8f29482739ac16d4a2a4b03eeff997560737ba
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2014-03-10 11:20:59 +0100

    X, y coordinates for trackBox

--- Aflevering-3/VectorRally.java
+++ Aflevering-3/VectorRally.java
@@ -1,20 +1,21 @@
 public class VectorRally {
     public static void main(String[] args) {
-        drawTrackBox(20, 20);
+        drawTrackBox(20, 20, 50, 50);
     }
 
-    public static void drawTrackBox(int width, int height) {
+    public static void drawTrackBox(int x, int y, int width, int height) {
         StdDraw.setXscale(0, 50);
         StdDraw.setYscale(0, 50);
         StdDraw.setPenRadius(1/500.);
+        StdDraw.setPenColor(StdDraw.DARK_GRAY);
 
         for (int i = 0; i < width; i++) {
-            StdDraw.line(0 + i, 0, 0 + i, height);
+            StdDraw.line(x + i, y, x + i, y + height);
         }
 
 
         for (int i = 0; i < height; i++) {
-            StdDraw.line(0, 0 + i, width, 0 + i);
+            StdDraw.line(x, y + i, x + width, y + i);
         }
     }
 }
