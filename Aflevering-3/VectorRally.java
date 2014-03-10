a3eeb0000b123b71709b097459f7ab3038561e8b
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2014-03-10 11:16:46 +0100

    drawTrackBox method

--- Aflevering-3/VectorRally.java
+++ Aflevering-3/VectorRally.java
@@ -1,13 +1,20 @@
 public class VectorRally {
-	public static void main(String[] args) {
-		drawTrack();
-	}
+    public static void main(String[] args) {
+        drawTrackBox(20, 20);
+    }
 
-	public static void drawTrack() {
-		StdDraw.setXscale(0, 10);
-        StdDraw.setYscale(0, 10);
-        StdDraw.setPenRadius(1/100.);
+    public static void drawTrackBox(int width, int height) {
+        StdDraw.setXscale(0, 50);
+        StdDraw.setYscale(0, 50);
+        StdDraw.setPenRadius(1/500.);
 
-        StdDraw.line(0, 0,1,1);
-	}
+        for (int i = 0; i < width; i++) {
+            StdDraw.line(0 + i, 0, 0 + i, height);
+        }
+
+
+        for (int i = 0; i < height; i++) {
+            StdDraw.line(0, 0 + i, width, 0 + i);
+        }
+    }
 }
