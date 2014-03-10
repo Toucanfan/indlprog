f1d0b4e5de98963232964ebcf02d9191c034b059
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2014-03-10 11:36:09 +0100

    Obstacle box graphics

--- Aflevering-3/VectorRally.java
+++ Aflevering-3/VectorRally.java
@@ -1,6 +1,7 @@
 public class VectorRally {
     public static void main(String[] args) {
-        drawTrackBox(0, 0, 20, 50);
+        drawTrackBox(0, 0, 50, 50);
+        drawObstacleBox(12, 12, 25, 25);
     }
 
     public static void drawTrackBox(int x, int y, int width, int height) {
@@ -28,5 +29,17 @@ public class VectorRally {
         for (int i = 0; i < height; i++) {
             StdDraw.line(x, y + i, x + width, y + i);
         }
+    }
+
+    public static void drawObstacleBox(int x, int y, int width, int height) {
+        /*
+         * Draws an obstacle (not drivable) background.
+         */
+        StdDraw.setPenColor(StdDraw.WHITE);
+        StdDraw.filledRectangle(x + (width / 2.), y + (height / 2.), width / 2., height / 2.);
+
+        StdDraw.setPenColor(StdDraw.DARK_GRAY);
+        StdDraw.setPenRadius(1/250.);
+        StdDraw.rectangle(x + (width / 2.), y + (height / 2.), width / 2., height / 2.);
     }
 }
