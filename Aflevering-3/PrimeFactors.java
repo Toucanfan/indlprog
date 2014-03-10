84c0de16c4acb3782aa7b7e73a71506082b08729
heyer2 <danielsafari@yahoo.dk>
2014-03-10 20:36:19 +0100

    Lavet opgave 1

---
+++ Aflevering-3/PrimeFactors.java
@@ -0,0 +1,53 @@
+import java.util.*;
+	
+public class PrimeFactors {
+
+public static String primeList = "2"; //We initialize the list to 2 in order to simplify the code
+public static long maxNumCheckedIfPrime = 2;
+	
+public static void main(String[] args) {
+	Scanner console = new Scanner(System.in);
+	while(true) {
+		System.out.print("Enter an integer greater than 1: ");
+		while (!console.hasNextLong()) {
+			System.out.print("Invalid input, please try again: ");
+			console.next();
+		}
+		printPrimeFactors(console.nextLong());
+	}	
+}
+
+	public static void expandPrimeList(long expandTarget) {
+		Scanner primeLookup = null;
+		boolean isPrime;
+		for (long potentialPrime=maxNumCheckedIfPrime+1; potentialPrime<=expandTarget; potentialPrime++) {
+			isPrime = true; // Assumed until disproven
+			primeLookup = new Scanner(primeList);
+			while (primeLookup.hasNext())
+				if (potentialPrime%primeLookup.nextLong() == 0) {
+					isPrime = false;
+					break;
+					}
+			if (isPrime)
+					primeList += " " + potentialPrime;
+		}
+		maxNumCheckedIfPrime = expandTarget;
+	}
+
+	public static void printPrimeFactors(long toBeFactored) {
+		if (toBeFactored>maxNumCheckedIfPrime)
+			expandPrimeList(toBeFactored);
+		Scanner primeLookup = new Scanner(primeList);
+		String primeFactors = "";
+		long potentialFactor = 0;
+		while (potentialFactor<=toBeFactored) {
+			potentialFactor = primeLookup.nextLong();
+			while (toBeFactored%potentialFactor == 0) {
+				primeFactors += potentialFactor + ", ";
+				toBeFactored /= potentialFactor;
+			}
+		}
+		// Eclipse does not handle the escape command "\b", despite it being a standardized part of java. Test by some other means
+		System.out.println("The prime factors are: [" + primeFactors + "\b\b]\n"); 
+	}
+}
