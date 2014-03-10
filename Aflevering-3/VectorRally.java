3af7a725aacd1f1320e8bbfb419c8bbe52e942a8
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2014-03-10 11:30:36 +0100

    Pretty colors and nice looks

--- Aflevering-3/VectorRally.java
+++ Aflevering-3/VectorRally.java
@@ -1,13 +1,24 @@
 public class VectorRally {
     public static void main(String[] args) {
-        drawTrackBox(20, 20, 50, 50);
+        drawTrackBox(0, 0, 20, 50);
     }
 
     public static void drawTrackBox(int x, int y, int width, int height) {
+        /*
+         * Draws a track (drivable) background.
+         */
         StdDraw.setXscale(0, 50);
         StdDraw.setYscale(0, 50);
-        StdDraw.setPenRadius(1/500.);
+
+        StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
+        StdDraw.filledRectangle(x + (width / 2.), y + (height / 2.), width / 2., height / 2.);
+
         StdDraw.setPenColor(StdDraw.DARK_GRAY);
+        StdDraw.setPenRadius(1/250.);
+        StdDraw.rectangle(x + (width / 2.), y + (height / 2.), width / 2., height / 2.);
+
+        StdDraw.setPenRadius(1/500.);
+        StdDraw.setPenColor(StdDraw.GRAY);
 
         for (int i = 0; i < width; i++) {
             StdDraw.line(x + i, y, x + i, y + height);
