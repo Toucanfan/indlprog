6fd079ee8ca895e07ffa129d1f99998f7e8962c2
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2014-03-25 10:25:47 +0100

    Graphics

---
+++ Aflevering-4/Gol/GameOfLifeMain.java
@@ -0,0 +1,37 @@
+public class GameOfLifeMain {
+	public static GameOfLife gol;
+
+	public static void main(String[] args) {
+		int size = 100;
+
+		gol = new GameOfLife(size);
+
+		gol.world[5][5] = 1;
+		gol.world[5][4] = 1;
+		gol.world[5][3] = 1;
+		gol.world[4][3] = 1;
+		gol.world[3][4] = 1;
+
+		StdDraw.setXscale(0,size);
+		StdDraw.setYscale(0,size);
+        StdDraw.setPenRadius(1/(size * 1.));
+
+        while(true) {
+        	StdDraw.show(100);
+        	render();
+        	gol.nextState();
+        }
+	}
+
+	public static void render(){
+        StdDraw.clear();
+		for (int i = 0; i < gol.world.length; i++) {
+        	int[] row = gol.world[i];
+        	for (int j = 0; j < row.length; j++) {
+        		if(1==gol.world[i][j]) {
+        			StdDraw.point(i, j);
+        		}
+        	}
+        }
+	}
+}
